43d969fef6470c337ed8a0ff39086fbf54cb8a1f
vinit <vinitkumar93341@gmail.com>
2026-06-18 18:19:42 +0530

    feat: add Clerk webhook handler for user events; integrate with Express server

---
+++ backend/src/webhooks/clerk.webhook.js
@@ -0,0 +1,56 @@
+import express from "express";
+import User from "../models/user.model.js";
+import { verifyWebhook } from "@clerk/backend/webhooks";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const signingSecret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+    if (!signingSecret) {
+      return res.status(503).json({
+        message: "Webhook secret is not provided",
+      });
+    }
+    const payload = Buffer.isBuffer(req.body)
+      ? req.body.toString("utf8")
+      : String(req.body);
+    const request = new Request("http://blabla/webhooks/clerk", {
+      method: "POST",
+      headers: new Headers(req.headers),
+      body: payload,
+    });
+
+    const evt = await verifyWebhook(request, { signingSecret });
+
+    //after verifying webhook if eventt type is user created or updated
+    if (evt.type === "user.created" || evt.type === "user.updated") {
+      const u = evt.data;
+      const email =
+        u.email_addresses?.find((e) => e.id === u.primary_email_address_id)
+          ?.email_address ?? u.email_addresses?.[0]?.email_address;
+
+      const fullName =
+        [u.first_name, u.last_name].filter(Boolean).join(" ") ||
+        u.username ||
+        email?.split("@")[0];
+
+      await User.findOneAndUpdate(
+        { clerkId: u.id },
+        { clerkId: u.id, email, fullName, profilePic: u.image_url },
+      );
+    }
+
+    //if even type is user deleted
+    if (evt.type === "user.deleted") {
+      if (evt.data.id) await User.findOneAndDelete({ clerkId: evt.data.id });
+    }
+
+    return res.status(200).json({ received: true });
+  } catch (error) {
+    console.error("Error in Clerk webhook:", error);
+    return res.status(400).json({ message: "Webhook verification failed" });
+  }
+});
+
+export default router;
